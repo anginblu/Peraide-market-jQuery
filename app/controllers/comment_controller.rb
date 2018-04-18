83db512000a358e289102781a64e6f204bee7306
Jacqueline <anginblu@gmail.com>
2018-04-17 20:34:14 -0400

    update comment_controller  via jQuery and an Active Model Serialization JSON Backend

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,19 +1,37 @@
 class CommentsController < ApplicationController
 
-  before_action :set_profile, only: [:index, :show]
-  before_action :set_user, only: [:create]
+  before_action :set_profile, only: [:index, :show, :update]
 
   def index
-    @comments = @profile.comments
+    comments = @profile.comments
+    render json: comments
     # binding.pry
   end
 
   def show
-    @comment = @profile.profile
+    @comment = @profile.comments.find(params[:id])
+    render json: @comment
+  end
+
+  def new
+    if !current_user
+      redirect_to root_path, alert: "Please log in first."
+    end
   end
 
   def create
+    @comment = @profile.comments.new(comment_params)
+    @comment.user_id = current_user
+    if @comment.save
+      render json: comment, status: 201
+    else
+      render
+  end
 
+  def update
+    @comment = @profile.comments.find(params[:id])
+    @comment.update(comment_params)
+    render json: @comment
   end
 
   private
@@ -23,6 +41,10 @@ class CommentsController < ApplicationController
 
   def set_user
     @user= current_user
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
   end
 
 end
